9c6056bfe21d329e66895ad523aca1dddc520a98
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-09-09 19:45:44 +0700

    add shell functionality

--- genaf_base/scripts/shell.py
+++ genaf_base/scripts/shell.py
@@ -1,11 +1,29 @@
-
-import readline
+from rhombus.lib.utils import cerr, get_dbhandler
+from rhombus.scripts import setup_settings, arg_parser
+from genaf_base.scripts import run
+import sys
 
 def greet():
-    pass
+    cerr('genaf-base-shell - shell for genaf-base')
+
 
 def usage():
-    pass
+    cerr('genaf-base-shell - shell for genaf-base')
+    cerr('usage:')
+    cerr('\t%s scriptname [options]' % sys.argv[0])
+    sys.exit(0)
+
 
 def main():
-    pass
+    greet()
+
+    # preparing everything
+    p = arg_parser('genaf-base-shell')
+    args = p.parse_args(sys.argv[1:])
+
+    settings = setup_settings( args )
+    dbh = get_dbhandler(settings)
+
+    from IPython import embed
+    import transaction
+    embed()
